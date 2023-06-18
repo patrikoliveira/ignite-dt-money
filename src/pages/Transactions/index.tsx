f6ddff3b981d4841ca5dd19897a03ce641fe9179
Patrik Oliveira <patrikro.oliveira@gmail.com>
2023-06-18 20:16:06 -0300

    feat: :sparkles: projeto dt-money pra treinamento de consumo de apis com react + estudo de performance

---
+++ src/pages/Transactions/index.tsx
@@ -0,0 +1,50 @@
+import { Header } from '../../components/Header'
+import { Summary } from '../../components/Summary'
+import { SearchForm } from './components/SearchForm'
+import {
+  PriceHighlight,
+  TransactionContainer,
+  TransactiosTable,
+} from './styles'
+import { TransactionsContext } from '../../contexts/TransactionsContext'
+import { dateFormatter, priceFormatter } from '../../utils/formatter'
+import { useContextSelector } from 'use-context-selector'
+
+export const Transactions = () => {
+  const transactions = useContextSelector(
+    TransactionsContext,
+    (context) => context.transactions,
+  )
+
+  return (
+    <div>
+      <Header />
+      <Summary />
+
+      <TransactionContainer>
+        <SearchForm />
+        <TransactiosTable>
+          <tbody>
+            {transactions.map((transaction) => {
+              return (
+                <tr key={transaction.id}>
+                  <td width="50%">{transaction.description}</td>
+                  <td>
+                    <PriceHighlight variant={transaction.type}>
+                      {transaction.type === 'outcome' && '- '}
+                      {priceFormatter.format(transaction.price)}
+                    </PriceHighlight>
+                  </td>
+                  <td>{transaction.category}</td>
+                  <td>
+                    {dateFormatter.format(new Date(transaction.createdAt))}
+                  </td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </TransactiosTable>
+      </TransactionContainer>
+    </div>
+  )
+}
